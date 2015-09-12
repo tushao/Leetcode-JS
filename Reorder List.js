abe96c392beb115e5f142c9d0a31dcb622ecf669
tushao <yt935@nyu.edu>
2015-09-11 23:47:35 -0400

    time limited

---
+++ Reorder List.js
@@ -0,0 +1,79 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+
+//把数组分成两个数组(快慢指针)
+function findMiddle(head) {
+    var slow = head;
+    if (head !== null) {
+        var fast = head.next;
+    while(fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    }
+    return slow;
+}
+
+//反转第二段链表
+function reverse(head) {
+    if (head === null) {
+        return head;
+    }
+    var premNode = head;
+    var nNode = premNode.next;
+    head.next = null;
+    while (nNode !== null) {
+        var temp = nNode.next;
+        nNode.next = premNode;
+        premNode = nNode;
+        nNode = temp
+    }
+    return premNode;
+}
+
+//两个list进行merge
+function merge(head1, head2) {
+    var index = 0;
+    var dummyNode = new ListNode(0);
+    while (head1 !== null && head2 !== null) {
+        if (index % 2 === 0) {
+            dummyNode.next = head1;
+            head1 = head1.next;
+        }
+        else {
+            dummyNode.next = head2;
+            head2 = head2.next;
+        }
+        dummyNode = dummyNode.next;
+        index++;
+    }
+    if (head1 !== null) {
+        dummyNode.next = head1;
+    }
+    else {
+        dummyNode.next = head2;
+    }
+}
+// 主函数
+var reorderList = function(head) {
+    var mid = findMiddle(head);
+    if (mid !== null) {
+        var tail = reverse(mid.next);
+        merge(head, tail);
+    }
+    else {
+        merge(head, null)
+    }
+    //mid.next = null;
+    
+    
+};
