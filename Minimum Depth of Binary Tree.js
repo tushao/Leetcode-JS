c103643bb9e70ca72b008e5cb79451246b0cbee6
tushao <yt935@nyu.edu>
2016-10-13 01:23:11 -0700

    HW

---
+++ Minimum Depth of Binary Tree.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if (!root) return 0
+    var L = minDepth(root.left), R = minDepth(root.right)
+    return 1 + (Math.min(L, R) || Math.max(L, R))
+};
